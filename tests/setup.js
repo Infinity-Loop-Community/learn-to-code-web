7e72f65e9f31d7ffa88a4cf428faae4655b11392
Sebastian Sigl <sebastian.sigl@adevinta.com>
2023-10-20 08:02:17 +0200

    Try setting the env to test for the testing stage

--- tests/setup.js
+++ tests/setup.js
@@ -1,17 +1,6 @@
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 
-// hide error messages about act() being unsupported in production build
-const ignoredErrors = [
-  /act(...) is not supported in production builds of React, and might not behave as expected./,
-]
-const consoleError = global.console.error
-global.console.error = (...args) => {
-  if (!ignoredErrors.find((el) => el.test(args[0]))) {
-    return consoleError(...args)
-  }
-}
-
 afterEach(() => {
   cleanup();
 });
